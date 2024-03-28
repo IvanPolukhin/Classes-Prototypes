025dd2a80104a3e1c8bbdde815ed37411b91acfc
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-03-28 22:43:54 +0200

    Classes in Practice

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-class Animal {
-    constructor(name) {
-        this.name = name
+class ListBinding {
+    constructor(element) {
+        this.listElement = element;
+        this.textList = [];
     }
-    makeSound() {
-        console.log('Generic Animal Sound!!');
+    // Makes an <li></li> element/tag
+    static createListItem(text) {
+        const li = document.createElement('li');
+        li.textContent = text;
+        return li;
+    }
+
+    update() {
+        /* Remove all existing <li> elements/tags */
+        while (this.listElement.firstChild) {
+            this.listElement.removeChild(this.listElement.firstChild);
+        }
+        /* Fill <ul>/<ol> tag with <li> */
+        for (const text of this.textList) {
+            this.listElement.appendChild(ListBinding.createListItem(text));
+        }
+    }
+
+    add(text) {
+        this.textList.push(text);
+        this.update();
+    }
+
+    remove(index) {
+        this.textList.splice(index, 1);
+        this.update();
     }
 }
-
-class Dog extends Animal {
-    constructor(name) {
-        super(name);
-    }
-
-    makeSound() {
-        console.log('WOOF!! WOOF!!');
-    }
-}
-
-const a1 = new Animal('Dom');
-const a2 = new Dog('Jeff')
-
-a1.makeSound();
-a2.makeSound();
